9459641ba5e25f3945f4aa76219b9198b619c532
Parteek <sparteek9@gmail.com>
2021-10-16 22:33:58 +0530

    rchanges10

--- caesar/settings.py
+++ caesar/settings.py
@@ -114,5 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
 
